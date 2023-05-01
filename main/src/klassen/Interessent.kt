a352e01b3b690dcc66109be123069fa5378f54d8
AndreasZechSolutions <113876429+AndreasZechSolutions@users.noreply.github.com>
2023-05-01 20:38:58 +0200

    Update

--- main/src/klassen/Interessent.kt
+++ main/src/klassen/Interessent.kt
@@ -2,8 +2,10 @@ package klassen
 
 class Interessent(
     kontoId: Int = 0,
+    ausweisNr: String = "",
     kontoBezeichnung: KontoTyp,
     email: String = "",
     passwort: String = "",
-    var rolle: Anrede): Konto(kontoId, email, passwort) {
+    var rolle: Anrede,
+): Konto(kontoId, ausweisNr, kontoBezeichnung, email, passwort) {
 }
