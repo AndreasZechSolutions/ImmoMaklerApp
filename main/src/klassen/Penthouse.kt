3c8467e945ec250ed756d7e8888bb182cc21175c
AndreasZechSolutions <113876429+AndreasZechSolutions@users.noreply.github.com>
2023-05-03 21:40:47 +0200

    update

--- main/src/klassen/Penthouse.kt
+++ main/src/klassen/Penthouse.kt
@@ -1,4 +1,27 @@
 package klassen
 
-class Penthouse {
+class Penthouse(
+    immoId: Int = 0,
+    objektNr: String = "",
+    gesamtFlaeche: Double = 0.0,
+    preisNetto: Double = 0.0,
+    provision: Double = 0.0,
+    steuer: Int = 19,
+    typ: ImmoTyp,
+    art: ImmoArt,
+    dachTyp: Dach,
+    heizungsTyp: Heizung,
+    eAusweis: EnergieAusweis,
+    abwasser: String = "",
+    frei: String = "",
+    ort: String = "",
+    kreis: String = "",
+    bundesland: BlandAbk,
+    region: Land,
+    link_expose: String = "",
+    link_images: List<String>,
+    objektTyp: String = "Wohnung",
+
+    var whgTyp: String = "Penthouse"
+): Haus(immoId,objektNr,gesamtFlaeche,preisNetto,provision,steuer,typ,art,dachTyp,heizungsTyp,eAusweis,abwasser,frei,ort,kreis,bundesland,region,link_expose,link_images,objektTyp) {
 }
--- main/src/klassen/Whg.kt
+++ main/src/klassen/Whg.kt
@@ -1,6 +1,6 @@
 package klassen
 
-class Whg(
+open class Whg(
     immoId: Int = 0,
     objektNr: String = "",
     gesamtFlaeche: Double = 0.0,
@@ -21,6 +21,6 @@ class Whg(
     link_expose: String = "",
     link_images: List<String>,
 
-    var klasse: String = "Wohnung"
+    var objektTyp: String = "Wohnung"
 ): Immo(immoId,objektNr,gesamtFlaeche,preisNetto,provision,steuer,typ,art,dachTyp,heizungsTyp,eAusweis,abwasser,frei,ort,kreis,bundesland,region,link_expose,link_images) {
 }
