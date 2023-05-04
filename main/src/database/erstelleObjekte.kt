7a834e72231cf3c0e6ba8f4f80ec36070357ae75
AndreasZechSolutions <113876429+AndreasZechSolutions@users.noreply.github.com>
2023-05-04 11:44:32 +0200

    Update

---
+++ main/src/database/erstelleObjekte.kt
@@ -0,0 +1,9 @@
+package klassen.utils.database
+
+import klassen.Altbauwohnung
+import klassen.Bauernhof
+
+fun erstelleObjekte() {
+
+}
+
---
+++ main/src/utils/erstelleAltbauwohnung.kt
@@ -0,0 +1,6 @@
+package utils
+
+fun erstelleAltbauwohnung():  {
+
+
+}
