731fc845ad19fcd0b9039a70109917a9276d221e
AndreasZechSolutions <113876429+AndreasZechSolutions@users.noreply.github.com>
2023-05-09 11:20:27 +0200

    update

--- main/src/Main.kt
+++ main/src/Main.kt
@@ -1,9 +1,8 @@
 package klassen.utils
 
-import klassen.Immo
 
 // Globale Variablen
-var objekte = mutableListOf<Immo>()  // Immobilienobjekte
+
 
 fun main() {
 
---
+++ main/src/database/anlegenMakler.kt
@@ -0,0 +1,5 @@
+package klassen.utils.database
+
+fun anlegenMakler() {
+
+}
