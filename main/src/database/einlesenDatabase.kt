c6a939ea9a0c903218704eb054a949ac7aa75524
AndreasZechSolutions <113876429+AndreasZechSolutions@users.noreply.github.com>
2023-05-10 16:34:09 +0200

    update

--- main/src/database/einlesenDatabase.kt
+++ main/src/database/einlesenDatabase.kt
@@ -1,23 +1,13 @@
 package klassen.utils.database
 
-import klassen.Immo
-import klassen.Einfamilien
-import klassen.ObjektArt
-import klassen.Waehrung
-import klassen.ImmoTyp
-import klassen.ImmoArt
-import klassen.Dach
-import klassen.Heizung
-import klassen.EnergieAusweis
-import klassen.BlandAbk
-import klassen.Land
+import klassen.*
 
 fun einlesenDatabase() {
 
     var objekte = mutableListOf<Immo>()
 
     objekte.add(0, Einfamilien(
-        0,
+        1,
         "BBI-1",
         ObjektArt.Altbau,
         "ca. 1938",
@@ -60,4 +50,91 @@ fun einlesenDatabase() {
         "",
         ))
 
+    objekte.add(1, Mehrfamilien(
+        2,
+        "BBI-2",
+        ObjektArt.Altbau,
+        "1901",
+        501.00,
+        449000.00,
+        Waehrung.EUR,
+        3.57,
+        19,
+        ImmoTyp.Kauf,
+        ImmoArt.Privat,
+        Dach.Satteldach,
+        Heizung.Gas,
+        EnergieAusweis.inVorbereitung,
+        "139,2 kWH",
+        "",
+        "",
+        "Fürstenberg/Havel",
+        "Oberhavel",
+        BlandAbk.BB,
+        Land.Deutschland,
+        "",
+        "",
+        "Haus",
+        "",
+        "",
+        "",
+        "",
+        "5 Bäder",
+        "",
+        "50 % unterkellert, Kappendecke",
+        "Gastherme",
+        "",
+        "",
+        1523.00,
+        0.00,
+        18276.00,
+        23940.00,
+        "Mehrfamilienhaus",
+        "Mehrfamilienhaus in ruhiger Parklage",
+        "",
+    ))
+
+    objekte.add(2, Mehrfamilien(
+        3,
+        "BBI-3",
+        ObjektArt.Neubau,
+        "2010",
+        82.00,
+        60700.00,
+        Waehrung.EUR,
+        3.57,
+        19,
+        ImmoTyp.Kauf,
+        ImmoArt.Privat,
+        Dach.Satteldach,
+        Heizung.Gas,
+        EnergieAusweis.inVorbereitung,
+        "",
+        "",
+        "",
+        "",
+        "",
+        BlandAbk.BB,
+        Land.Deutschland,
+        "",
+        "",
+        "Haus",
+        "",
+        "",
+        "3,00 m",
+        "3 OG",
+        "",
+        "",
+        "",
+        "Zentral, Fußbodenheizung",
+        "",
+        "Ja, 35.000 EUR",
+        0.00,
+        0.00,
+        0.00,
+        0.00,
+        "Mehrfamilienhaus",
+        "Repräsentative Luxuswohnung",
+        "Perfekte Citywohnung mit Designerküche, Balkon und Stellplatz in 1A Kiez nördlich der Torstraße. Diese Wohnung kann als Kapitalanlage oder als private Cityresidenz erworben werden.",
+    ))
 }
