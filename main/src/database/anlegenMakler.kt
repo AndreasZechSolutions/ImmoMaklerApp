e5e20a85fbfd7465a1584b31ca0090d9823cf0d9
AndreasZechSolutions <113876429+AndreasZechSolutions@users.noreply.github.com>
2023-05-11 00:28:04 +0200

    update

--- main/src/database/anlegenMakler.kt
+++ main/src/database/anlegenMakler.kt
@@ -23,7 +23,7 @@ fun anlegenMakler(index: Int): MutableList<Makler> {
         "Bettina Bastgen",
         "Auguststraße",
         "36",
-        "Büro Berlin",
+        "",
         "10119",
         "Berlin",
         "030 - 28 04 03 09",
@@ -31,14 +31,18 @@ fun anlegenMakler(index: Int): MutableList<Makler> {
         "",
         "bastgen@bastgen-berlin.com",
         "www.bastgen-berlin.com",
-        "Bettina Bastgen ist Ihre persönliche Immobilienmaklerin für Berlin & Neuruppin\n" +
-                "Die Vermittlung hochwertiger und exklusiver Privat-Immobilien, insbesondere im Raum Neuruppin und Berlin, sind unser Spezialgebiet als Ihr persönlicher Immobilienmakler. An unseren beiden Standorten in Neuruppin und Berlin Mitte beraten wir Interessenten und Kunden ausführlich zu aktuellen Angeboten.\n" +
-                "\n" +
-                "Gerne begleiten wir als Makler mit juristischem Sachverstand den Verkauf Ihrer Immobilie. Wir vermitteln Häuser und Grundstücke, Landhäuser, Bauernhöfe, Einfamilienhäuser oder Mehrfamilienhäuser auch als Kapitalanlagen in Neuruppin, Berlin und Brandenburg.\n" +
-                "\n" +
-                "Bastgen Berlin Immobilien ist ein persönlich geführtes Unternehmen mit über 10 Jahren Erfahrung als Immobilienmaklerin.\n" +
-                "\n" +
-                "Ihre ganzheitliche und professionelle Beratung vor und nach dem Verkauf steht bei uns im Vordergrund. Hochwertige Privat-Immobilien von Ihrem Immobilienmakler in Berlin-Neuruppin."
+        "Bettina Bastgen ist Ihre persönliche Immobilienmaklerin für Berlin & Neuruppin.\n\n" +
+            "Die Vermittlung hochwertiger und exklusiver Privat-Immobilien, insbesondere im\n" +
+            "Raum Neuruppin und Berlin, sind unser Spezialgebiet als Ihr persönlicher Immobilienmakler.\n\n" +
+            "An unseren beiden Standorten in Neuruppin und Berlin Mitte beraten wir Interessenten und\n" +
+            "Kunden ausführlich zu aktuellen Angeboten.\n\n" +
+            "Gerne begleiten wir als Makler mit juristischem Sachverstand den Verkauf Ihrer Immobilie.\n\n" +
+            "Wir vermitteln Häuser und Grundstücke, Landhäuser, Bauernhöfe, Einfamilienhäuser oder\n" +
+            "Mehrfamilienhäuser auch als Kapitalanlagen in Neuruppin, Berlin und Brandenburg.\n\n" +
+            "Bastgen Berlin Immobilien ist ein persönlich geführtes Unternehmen mit über 10 Jahren\n" +
+            "Erfahrung als Immobilienmaklerin.\n\n" +
+            "Ihre ganzheitliche und professionelle Beratung vor und nach dem Verkauf\n" +
+            "steht bei uns im Vordergrund. Hochwertige Privat-Immobilien von Ihrem Immobilienmakler in Berlin-Neuruppin."
         ))
     return maklerListe
 }
--- main/src/utils/ausgabeKonsoleMakler.kt
+++ main/src/utils/ausgabeKonsoleMakler.kt
@@ -12,11 +12,38 @@ fun ausgabeKonsoleMakler(index: Int, maklerListe: MutableList<Makler>) {
     println(maklerListe[index].firmaTyp)
 
     if((maklerListe[index].vorname != "") && (maklerListe[index].nachname != "") )
-        print(maklerListe[index].vorname + " " + println(maklerListe[index].nachname))
+        println(maklerListe[index].vorname + " " + maklerListe[index].nachname)
 
     if((maklerListe[index].strasse != "") && (maklerListe[index].nummer != ""))
-        print(maklerListe[index].strasse + " " + maklerListe[index].nummer  + " " + maklerListe[index].zusatz)
+        println(maklerListe[index].strasse + " " + maklerListe[index].nummer  + " " + maklerListe[index].zusatz)
+
+    if((maklerListe[index].postleitzahl != "") && (maklerListe[index].ort != ""))
+        println(maklerListe[index].postleitzahl + " " + maklerListe[index].ort)
 
 
+    for(i in 1..2)
+        print("\n")
 
+    if(maklerListe[index].festnetz != "")
+        println("Festnetz: " + maklerListe[index].festnetz)
+
+    if(maklerListe[index].mobil != "")
+        println("Mobil: " + maklerListe[index].mobil)
+
+    if(maklerListe[index].fax != "")
+        println("Fax: " + maklerListe[index].fax)
+
+    if(maklerListe[index].eMail != "")
+        println("E-Mail: " + maklerListe[index].eMail)
+
+    if(maklerListe[index].web != "")
+        println("Web: " + maklerListe[index].web)
+
+    for(i in 1..2)
+        print("\n")
+
+    if(maklerListe[index].bio != "")
+        println("Über uns:\n" +
+                "=========\n" +
+                maklerListe[index].bio)
 }
