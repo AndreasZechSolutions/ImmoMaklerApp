c56fcc0005d7c088b6597313fa6d1b42a44d6dac
AndreasZechSolutions <113876429+AndreasZechSolutions@users.noreply.github.com>
2023-05-11 10:47:31 +0200

    update

--- main/src/utils/ausgabeKonsoleObjekte.kt
+++ main/src/utils/ausgabeKonsoleObjekte.kt
@@ -7,7 +7,7 @@ fun ausgabeKonsoleObjekte(index: Int, objektListe: List<Immo>)  {
     if(objektListe[index].objektNr != "")
         println("ObjektNr.: ${objektListe[index].objektNr} ")
 
-    println("ObjektArt.: ${objektListe[index].immoObjektArt} ")
+    println("Objektart.: ${objektListe[index].immoObjektArt} ")
 
     if(objektListe[index].bauJahr != "")
         println("Baujahr: ${objektListe[index].bauJahr} ")
@@ -23,6 +23,45 @@ fun ausgabeKonsoleObjekte(index: Int, objektListe: List<Immo>)  {
 
     if(objektListe[index].steuer != 0)
         println("Mehrwertsteuer:  ${objektListe[index].steuer} ")
+
+    println("Kauf/Miete: ${objektListe[index].typ} ")
+
+    println("Privat/Gewerbe: ${objektListe[index].art} ")
+
+    println("Dachtyp: ${objektListe[index].dachTyp} ")
+
+    println("Heizungstyp: ${objektListe[index].heizungsTyp} ")
+
+    println("Energie-Ausweis: ${objektListe[index].eAusweis} ")
+
+    if(objektListe[index].energierWert != "")
+        println("Energiewert: ${objektListe[index].energierWert} ")
+
+    if(objektListe[index].abwasser != "")
+        println("Abwasser: ${objektListe[index].abwasser} ")
+
+    if(objektListe[index].frei != "")
+        println("Frei ab: ${objektListe[index].frei} ")
+
+    if(objektListe[index].ort != "")
+        println("Frei ab: ${objektListe[index].ort} ")
+
+    println("Bundesland: ${objektListe[index].bundesland} ")
+
+    println("Bundesland: ${objektListe[index].region} ")
+
+    if(objektListe[index].linkExpose != "")
+        println("Frei ab: ${objektListe[index].linkImages} ")
+
+
+
+
+
+
+
+
+
+
 
 }
 
