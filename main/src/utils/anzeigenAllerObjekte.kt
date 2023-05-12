1509b0af00abe171a95ac7e448054c00bf00d9e4
AndreasZechSolutions <113876429+AndreasZechSolutions@users.noreply.github.com>
2023-05-12 10:44:23 +0200

    update

--- main/src/utils/anzeigenAllerObjekte.kt
+++ main/src/utils/anzeigenAllerObjekte.kt
@@ -10,8 +10,6 @@ fun anzeigenAllerObjekte(min: Int, max: Int, objektListe: List<Immo>) {
             for (i in 1..2)
                 println()
             if(i < max) {
-
-
                 println("Drücke eine Enter-/Return-Taste für weiter ...")
                 readln()
             }
