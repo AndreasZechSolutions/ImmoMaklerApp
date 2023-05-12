fa3874b9f6a05c9127d32a637cc319c90165f877
AndreasZechSolutions <113876429+AndreasZechSolutions@users.noreply.github.com>
2023-05-12 14:13:25 +0200

    update

--- main/src/utils/ausgabeKonsoleObjekte.kt
+++ main/src/utils/ausgabeKonsoleObjekte.kt
@@ -110,8 +110,10 @@ fun ausgabeKonsoleObjekte(index: Int, objektListe: List<Immo>)  {
             println("Bad: ${hausObjekt.nettoMieteSollJahr} ${objektListe[index].geldTyp}")
     }
 
-    if (objektListe[index].objektTyp == "Wohnung") {
-        val whgObjekt: Whg = objektListe[index] as Whg
+    if (index <= objektListe.size-1) {
+        if (objektListe[index].objektTyp == "Wohnung") {
+            val whgObjekt: Whg = objektListe[index] as Whg
+
 
         if (whgObjekt.renovierung != "")
             println("Renovierung: ${whgObjekt.renovierung} ")
